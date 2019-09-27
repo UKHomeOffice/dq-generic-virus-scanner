a0af239884492219f4e57bbf516e229b5adcadce
James Lizamore <jwlizamore@gmail.com>
2019-09-27 13:37:42 +0100

    Setup logger
    Update README

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # dq-generic-virus-scanner
 
-Repo for generic virus scanning process
+This repo houses a dockerized virus scanning solution which utilizes a ClamAV instance to virus scan
+files in a configured directory and output the files to a configured S3 bucket.
+
+## Usage
+
+This repo does not provide a file acquisition functionality. There is an example function `pull_files()` in `app/scripts/file_ingest.py` which simulates the acquisition of files for demonstration purposes. However for production usage you will be required to replace this with an appropriate function to pull files from your desired location.
+
+## Configuration
+
+
+| Environment Variable | Example        | Description                             | Required |
+|----------------------|----------------|-----------------------------------------|----------|
+| S3_BUCKET_NAME       | my_bucket      | Bucket to output clean files to         |  Yes     |
+| BUCKET_KEY_PREFIX    | my/path        | File prefix used when uploading to S3   |  Yes     |
+| S3_ACCESS_KEY_ID     | AK124512A      | Access key id for S3 Bucket             |  Yes     |
+| S3_SECRET_ACCESS_KEY | YKJADAS12      | Access key secret for S3 bucket         |  Yes     |
+| S3_REGION_NAME       | eu-west-2      | Region the S3 bucket resides in         |  Yes     |
+| BASE_URL             | localhost      | Base URL of clamav service              |  No      |
+| BASE_PORT            | 8080           | Port used for clamav service            |  No      |
+| INPUT_DIR            | /tmp/input     | Dir files will be acquired into         |  No      |
+| OUTPUT_DIR           | /tmp/output    | Dir files are placed in before upload   |  No      |
+| QUARANTINE_DIR       | /tmp/quarantine| Dir virus files are placed in           |  No      |
+
+## Local development
+
+Given the neccessary environment variables have been set. You can set the application up by:
+
+1. Install dependencies
+`pip install -r packages.txt`
+
+2. Starting up a local clamAv instance through docker-compose
+`docker-compose up`
+
+3. Starting the process by running
+`python scripts/file_ingest.py`
+
+## Deployment
+
+Kube files are provided for deployment including a persistant volume claim
+in order to prevent loss of files if moving files from an external location and the pod diess
--- app/scripts/file_ingest.py
+++ app/scripts/file_ingest.py
@@ -20,6 +20,14 @@ INPUT_DIR               = os.environ.get('INPUT_DIR', '/tmp/input')
 OUTPUT_DIR              = os.environ.get('OUTPUT_DIR', '/tmp/output')
 QUARANTINE_DIR          = os.environ.get('QUARANTINE_DIR', '/tmp/quarantine')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 class VirusScanner:
     """Class for interacting with a ClamAV vir scanning service"""
@@ -38,23 +46,23 @@ class VirusScanner:
         Returns: 
             None
         """
-        print('Starting virus scan')
+        logger.info('Starting virus scan')
         for file_name in os.listdir(self.input_dir):
-            print(f'Virus scanning {file_name}')
+            logger.info(f'Virus scanning {file_name}')
             with open(f'{self.input_dir}/{file_name}', 'rb') as file_contents:
                 response = requests.post('http://' + self.scanner_host + ':' + self.scanner_port + '/scan', files={'file': file_contents}, data={'name': file_name})
                 if not 'Everything ok : true' in response.text:
-                    print(f'Virus scan FAIL: {file_name} is dangerous! Moving to quarantine')
+                    logger.info(f'Virus scan FAIL: {file_name} is dangerous! Moving to quarantine')
                     self._move_file(file_name, self.quarantine_dir)
                     continue
                 else:
-                    print(f'Virus scan OK: {file_name}')
+                    logger.info(f'Virus scan OK: {file_name}')
                     self._move_file(file_name, self.output_dir)
-        print('Scan complete')
+        logger.info('Scan complete')
 
     def _create_dir(self, dir_location):
         if not os.path.isdir(dir_location):
-            print('Output dir does not exist, creating')
+            logger.info('Output dir does not exist, creating')
             os.mkdir(dir_location)
 
     def _move_file(self, file_name, output_dir):
@@ -64,7 +72,7 @@ class VirusScanner:
 def pull_files():
     """Temp function to simulate pulling of files
     """
-    for i in range(10):
+    for i in range(2):
         if not os.path.isdir(INPUT_DIR):
             os.mkdir(INPUT_DIR)
         with open(f'{INPUT_DIR}/file{i}', 'a') as f:
@@ -85,10 +93,11 @@ def move_file_s3(file_location):
             BUCKET_NAME,
             f'{BUCKET_KEY_PREFIX}/{today.year}/{today.month}/{today.day}/{file_name}'
         )
-        print(f'Uploaded: {file_name}')
+        logger.info(f'Uploaded: {file_name}')
     except Exception as err:
-        print(f'Failed to upload: {file_name}')
-        print(err)
+        logger.info(f'Failed to upload: {file_name}')
+        logger.iinfo(err)
+        raise
 
 def main():
     pull_files()
